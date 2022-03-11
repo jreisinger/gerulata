f029efde6a73a24e9c80bbf6e7b14b4b6a6ef436
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-03-11 14:53:02 +0100

    enrich data with URL path, IP addresses and AS description

--- parse.go
+++ parse.go
@@ -3,25 +3,38 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
+	"net"
+	"net/url"
 	"os"
+	"regexp"
 	"strconv"
+
+	"github.com/jreisinger/checkip/checks"
 )
 
 type Node struct {
-	ID        int    `json:"id"`
-	Title     string `json:"title"`
-	Type      string `json:"type"`
-	Activity  string `json:"activity"`
-	Impact    string `json:"impact"`
-	Influence string `json:"influence"`
-	Threat    string `json:"threat"`
+	ID        int      `json:"id"`
+	Title     string   `json:"title"`
+	Type      string   `json:"type"`
+	Activity  string   `json:"activity"`
+	Impact    string   `json:"impact"`
+	Influence string   `json:"influence"`
+	Threat    string   `json:"threat"`
+	UrlPath   string   `json:"url"`
+	IPs       []net.IP `json:"ip_addresses"`
+	AS        string   `json:"as"`
 }
 
+// var as = flag.Bool("as", false, "get AS description")
+
 func main() {
-	file, err := os.Open(os.Args[1])
+	flag.Parse()
+
+	file, err := os.Open(flag.Args()[0])
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,11 +45,34 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// if *as {
+	// 	a, err := getAS()
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println(a)
+
 	j, err := getJSON(nodesByID)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s", j)
+
+}
+
+func extractUrl(s string) (*url.URL, error) {
+	r := regexp.MustCompile(`\(([^)]+)\)`)
+	rawURL := r.FindStringSubmatch(s)[1]
+	return url.Parse(rawURL)
+}
+
+func getAS() (string, error) {
+	ip := net.ParseIP("1.1.1.1")
+	res, err := checks.CheckAS(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res.Info.JsonString()
 }
 
 func getJSON(nodesByID map[int]*Node) ([]byte, error) {
@@ -51,6 +87,9 @@ func getJSON(nodesByID map[int]*Node) ([]byte, error) {
 			Impact:    node.Impact,
 			Influence: node.Influence,
 			Threat:    node.Threat,
+			UrlPath:   node.UrlPath,
+			IPs:       node.IPs,
+			AS:        node.AS,
 		}
 		nodes = append(nodes, n)
 	}
@@ -89,6 +128,27 @@ func getNodesByID(r io.Reader) (map[int]*Node, error) {
 			nodesByID[id].Title = line
 		case n == 3:
 			nodesByID[id].Type = line
+			if nodesByID[id].Type == "Web" {
+				// Enrich with URL path
+				u, err := extractUrl(nodesByID[id].Title)
+				if err != nil {
+					return nil, err
+				}
+				nodesByID[id].UrlPath = u.Path
+
+				// Enrich with IP addresses
+				ips, _ := net.LookupIP(nodesByID[id].UrlPath) // NOTE: ignoring lookup error
+				nodesByID[id].IPs = ips
+
+				// Enrich with AS
+				if len(ips) != 0 {
+					res, err := checks.CheckAS(ips[0])
+					if err != nil {
+						return nil, err
+					}
+					nodesByID[id].AS = res.Info.Summary()
+				}
+			}
 		case n == 4:
 			nodesByID[id].Activity = line
 		case n == 5:
