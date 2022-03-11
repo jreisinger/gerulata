b0270f53a9749713305b2f163907a4be7c1feeb4
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-03-11 12:09:39 +0100

    move code to getNodesByID function

--- parse.go
+++ parse.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
@@ -25,45 +27,8 @@ func main() {
 	}
 	defer file.Close()
 
-	nodesByID := make(map[int]*Node)
-
-	var id int
-	var n int
-	input := bufio.NewScanner(file)
-	for input.Scan() {
-		line := input.Text()
-		if line == "" {
-			continue
-		}
-
-		n++
-		switch {
-		case n == 1: // new Node
-			id, err = strconv.Atoi(line)
-			if err != nil {
-				log.Fatalf("'%s' is not an ID (%v)", line, err)
-			}
-			_, ok := nodesByID[id]
-			if ok {
-				log.Fatalf("duplicate ID: %d", id)
-			}
-			nodesByID[id] = &Node{}
-		case n == 2:
-			nodesByID[id].Title = line
-		case n == 3:
-			nodesByID[id].Type = line
-		case n == 4:
-			nodesByID[id].Activity = line
-		case n == 5:
-			nodesByID[id].Impact = line
-		case n == 6:
-			nodesByID[id].Influence = line
-		case n == 7:
-			nodesByID[id].Threat = line
-			n = 0
-		}
-	}
-	if err := input.Err(); err != nil {
+	nodesByID, err := getNodesByID(file)
+	if err != nil {
 		log.Fatal(err)
 	}
 
@@ -86,4 +51,50 @@ func main() {
 	if err := encoder.Encode(&nodes); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getNodesByID(r io.Reader) (map[int]*Node, error) {
+	nodesByID := make(map[int]*Node)
+
+	var id int
+	var n int
+	var err error
+
+	input := bufio.NewScanner(r)
+
+	for input.Scan() {
+		line := input.Text()
+		if line == "" {
+			continue
+		}
+		n++
+
+		switch {
+		case n == 1: // new Node
+			id, err = strconv.Atoi(line)
+			if err != nil {
+				return nil, fmt.Errorf("'%s' is not an ID (%v)", line, err)
+			}
+			_, ok := nodesByID[id]
+			if ok {
+				return nil, fmt.Errorf("duplicate ID: %d", id)
+			}
+			nodesByID[id] = &Node{}
+		case n == 2:
+			nodesByID[id].Title = line
+		case n == 3:
+			nodesByID[id].Type = line
+		case n == 4:
+			nodesByID[id].Activity = line
+		case n == 5:
+			nodesByID[id].Impact = line
+		case n == 6:
+			nodesByID[id].Influence = line
+		case n == 7:
+			nodesByID[id].Threat = line
+			n = 0
+		}
+	}
+
+	return nodesByID, input.Err()
 }
